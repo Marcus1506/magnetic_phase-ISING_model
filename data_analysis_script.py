afaf9765eb35c2ae26e83b110db86f383d591072
Marcus1506 <116986601+Marcus1506@users.noreply.github.com>
2023-02-24 03:02:01 +0100

    first data exploration

--- data_analysis_script.py
+++ data_analysis_script.py
@@ -14,23 +14,26 @@ with np.load("data/ising.npz") as datafile:
 
 # build signmatrix for staggered magnetization
 # low staggered magnetization means strong grouping of magnetic regimes (weisssche bezirke)
-signmatrix=np.zeros(np.shape(spins))
+signmatrix=np.zeros((np.shape(spins)[1], np.shape(spins)[2]))
 for i in range(np.shape(spins)[1]):
     for j in range(np.shape(spins)[2]):
         signmatrix[i,j]=(-1)**(i+j)
 
+(L1,L2)=np.shape(spins[0,:,:])
+
 magn=[]
 smagn=[]
 for i in range(np.shape(spins)[0]):
-    magn.append(np.sum(spins[i])/(np.shape(spins)[1]*np.shape(spins)[2]))
-    temp=0
-    for j in range(np.shape(spins)[1]):
-        for k in range(np.shape(spins)[2]):
-            temp+=signmatrix[j,k]*spins[i,j,k]
-    smagn.append(temp/(np.shape(spins)[1]*np.shape(spins)[2]))
+    magn.append(np.sum(spins[i,:,:])/(L1*L2))
+    tempo=0
+    for j in range(L1):
+        for k in range(L2):
+            tempo+=signmatrix[j,k]*spins[i,j,k]
+    smagn.append(tempo/(L1*L2))
 
 magn=np.array(magn)
-smagn=np.array(smag)
+smagn=np.array(smagn)
+
 
 # visualizing the behaviour over temperature
 
@@ -40,4 +43,4 @@ plt.colorbar(label='temperature')
 plt.title("Scatterplot of temperature")
 plt.xlabel("magnetization")
 plt.ylabel("staggered magnetization")
-plt.show()
+plt.savefig("data/plots/scatterplot_over_temperature.png")
